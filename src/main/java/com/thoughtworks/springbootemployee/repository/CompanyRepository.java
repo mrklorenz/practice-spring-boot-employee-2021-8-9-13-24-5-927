9da23a34172ad27e75a4b6b4f259aea63741d185
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-12 02:39:18 +0800

    [SpringBootAPI][Mark] feat: Added more resource

--- src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/CompanyRepository.java
@@ -9,12 +9,12 @@ import java.util.List;
 @Repository
 public class CompanyRepository {
     private final List<Company> companies = new ArrayList<>();
-    private final EmployeeRepository employeeRepository = new EmployeeRepository();
 
     public CompanyRepository() {
-        companies.add(new Company(1, "OOCL", employeeRepository.getEmployees()));
-        companies.add(new Company(2, "COSCO", employeeRepository.getEmployees()));
-        companies.add(new Company(3, "MAERSK", employeeRepository.getEmployees()));
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+        companies.add(new Company(1, "OOCL", employeeRepository.getEmployees().subList(0,2)));
+        companies.add(new Company(2, "COSCO", employeeRepository.getEmployees().subList(2,4)));
+        companies.add(new Company(3, "MAERSK", employeeRepository.getEmployees().subList(4,6)));
     }
 
     public List<Company> getCompanies() {
--- src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/EmployeeRepository.java
@@ -12,9 +12,11 @@ public class EmployeeRepository {
 
     public EmployeeRepository() {
         employees.add(new Employee(1,"alice",20,"female",1000));
-        employees.add(new Employee(2,"bob",20,"male",1000));
-        employees.add(new Employee(3,"bobsy",20,"female",1000));
-        employees.add(new Employee(4,"mark",20,"male",1000));
+        employees.add(new Employee(2,"popoy",20,"male",1000));
+        employees.add(new Employee(3,"basha",20,"female",1000));
+        employees.add(new Employee(4,"karyn",20,"female",1000));
+        employees.add(new Employee(5,"sza",20,"female",1000));
+        employees.add(new Employee(6,"bruno",20,"male",1000));
     }
 
     public List<Employee> getEmployees() {
