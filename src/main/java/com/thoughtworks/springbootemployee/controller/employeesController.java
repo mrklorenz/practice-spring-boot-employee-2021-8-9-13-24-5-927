45932a0f8bc10c0015a02f28e733092402186989
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-11 17:18:18 +0800

    [employees][Kevin]refactor: rename employee to employees.java and move to model folder

--- src/main/java/com/thoughtworks/springbootemployee/controller/employeesController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/employeesController.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.controller;
 
 
+import com.thoughtworks.springbootemployee.model.Employees;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,18 +12,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/employees")
 public class employeesController {
-    List<Employee> employees = new ArrayList<>();
+    List<Employees> employees = new ArrayList<>();
 
     public employeesController() {
-        employees.add(new Employee(1,"alice",20,"female",1000));
-        employees.add(new Employee(2,"bob",20,"male",1000));
+        employees.add(new Employees(1,"alice",20,"female",1000));
+        employees.add(new Employees(2,"bob",20,"male",1000));
 
-        employees.add(new Employee(3,"bobsy",20,"female",1000));
-        employees.add(new Employee(4,"mark",20,"male",1000));
+        employees.add(new Employees(3,"bobsy",20,"female",1000));
+        employees.add(new Employees(4,"mark",20,"male",1000));
     }
 
     @GetMapping
-    public List<Employee> getAllEmployeeInfo()
+    public List<Employees> getAllEmployeeInfo()
     {
         return employees;
     }
