b6276e26511f845fc9aed8ab316a62a5284d213c
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-11 22:45:55 +0800

    [SpringBootAPI][Mark] feat: Added findEmployeesByGender API in controller class

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -4,10 +4,7 @@ package com.thoughtworks.springbootemployee.controller;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,11 @@ public class EmployeeController {
     @GetMapping(path = "/{employeeID}")
     public Employee findEmployeeByID(@PathVariable Integer employeeID) {
         return employeeService.findEmployeeById(employeeID);
+    }
+
+    @GetMapping(params = "gender")
+    public List<Employee> findEmployeesByGender(@RequestParam String gender) {
+        return employeeService.findEmployeesByGender(gender);
     }
 
 }
