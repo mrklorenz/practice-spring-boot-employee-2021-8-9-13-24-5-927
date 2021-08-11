3bc2377d85d3b156c1bf9b2e13c84214169d446e
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-12 01:37:39 +0800

    [SpringBootAPI][Mark] feat: added getters and setters in model class

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -1,4 +1,39 @@
 package com.thoughtworks.springbootemployee.model;
 
+import java.util.List;
+
 public class Company {
+    private Integer id;
+    private String name;
+    private List<Employee> employees;
+
+    public Company(Integer id, String name, List<Employee> employees) {
+        this.id = id;
+        this.name = name;
+        this.employees = employees;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 }
