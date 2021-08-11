c3fc3e87925b12a70afef51c2eeba16699a4ebf8
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-12 01:36:08 +0800

    [SpringBootAPI][Mark] feat: Added return for method and corrected annotations

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -1,11 +1,24 @@
 package com.thoughtworks.springbootemployee.service;
 
 import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CompanyService {
-    public List<Company> getAllCompanies() {
-        return null;
+    @Autowired
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
     }
+
+    public List<Company> getAllCompanies() {
+        return companyRepository.getCompanies();
+    }
+
+
 }
