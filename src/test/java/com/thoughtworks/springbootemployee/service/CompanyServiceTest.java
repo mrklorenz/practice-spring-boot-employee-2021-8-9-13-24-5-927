aa697cfc6992ebc6928f13269dbb92fa666096ff
balajma <mark.lorenz.balajadia@oocl.com>
2021-08-12 01:35:30 +0800

    [SpringBootAPI][Mark] test: Added test for get All Companies - should_return_all_companies_when_getAllCompanies_given_all_companies()

--- src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/CompanyServiceTest.java
@@ -2,12 +2,14 @@ package com.thoughtworks.springbootemployee.service;
 
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
@@ -20,6 +22,9 @@ public class CompanyServiceTest {
 
     @Mock
     private CompanyRepository companyRepository;
+
+    @Mock
+    private EmployeeRepository employeeRepository;
 
     @Test
     public void should_return_all_companies_when_getAllCompanies_given_all_companies() {
@@ -35,6 +40,10 @@ public class CompanyServiceTest {
     }
 
     private List<Company> generateCompanies() {
-        return null;
+        List<Company> companies = new ArrayList<>();
+        companies.add(new Company(1, "OOCL", employeeRepository.getEmployees()));
+        companies.add(new Company(2, "COSCO", employeeRepository.getEmployees()));
+        companies.add(new Company(3, "MAERSK", employeeRepository.getEmployees()));
+        return companies;
     }
 }
